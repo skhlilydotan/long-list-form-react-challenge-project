4d634bf6ca79e29527504cfd90b968ed31ec1399
micah66 <micahgordon66@gmail.com>
2023-10-19 01:13:37 +0300

    add functionality for editing user data fields

--- src/pages/users/userRow/UserRow.jsx
+++ src/pages/users/userRow/UserRow.jsx
@@ -1,3 +1,4 @@
+import { useReducer } from 'react';
 import { Grid } from '@mui/material';
 import InputField from '../../../components/InputField';
 import TrashIconButton from '../../../components/TrashIconButton';
@@ -6,21 +7,67 @@ import styles from '../users.module.css';
 // user country must be one of those - for select/autocomplete implementation
 import countryOptions from '../../../data/countries.json';
 
+const USER_ACTIONS = {
+  EDIT_FIELD: 'editTextField',
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case USER_ACTIONS.EDIT_FIELD: {
+      return {
+        ...state,
+        [action.payload.fieldName]: action.payload.inputValue,
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
 const UserRow = ({ user }) => {
+  const [state, dispatch] = useReducer(reducer, user);
+
+  const editTextField = (fieldName, inputValue) =>
+    dispatch({
+      type: USER_ACTIONS.EDIT_FIELD,
+      payload: { fieldName, inputValue },
+    });
+
   return (
     <Grid container className={styles.userRow} columnGap={1}>
       {/* Render each user row inputs and trash icon at the end of each row */}
       <Grid item xs>
-        <InputField name="name" value={user.name} placeholder="User Name" />
+        <InputField
+          name="name"
+          value={state.name}
+          placeholder="User Name"
+          onChangehandler={editTextField}
+        />
       </Grid>
       <Grid item xs>
-        <InputField name="country" value={user.country} placeholder="Country" />
+        <InputField
+          name="country"
+          value={state.country}
+          placeholder="Country"
+          onChangehandler={editTextField}
+        />
       </Grid>
       <Grid item xs>
-        <InputField name="email" value={user.email} placeholder="Email" />
+        <InputField
+          name="email"
+          value={state.email}
+          placeholder="Email"
+          onChangehandler={editTextField}
+        />
       </Grid>
       <Grid item xs>
-        <InputField name="phone" value={user.phone} placeholder="Phone Number" />
+        <InputField
+          name="phone"
+          value={state.phone}
+          placeholder="Phone Number"
+          onChangehandler={editTextField}
+        />
       </Grid>
       <Grid item xs="auto">
         <TrashIconButton />
